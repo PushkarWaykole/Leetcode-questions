ef39761eb2f78014ef6949b803c28f88f3dd6ef8
Pushkar Nitin Waykole <80080666+PushkarWaykole@users.noreply.github.com>
2022-11-30 08:15:17 +0530

    Time: 3 ms (86.20%), Space: 8 MB (63.54%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mpp;
+        for(auto it:arr){
+            mpp[it]++;
+        }
+        unordered_set<int> st;
+        for(auto it:mpp){
+            if(st.find(it.second)!=st.end()){
+                return false;
+            }
+            st.insert(it.second);
+        }
+        return true;
+    }
+};
